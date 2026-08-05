d2d17d45cfa03410f955c9612d769b2512b75b52
Emily LaBonty <emilylabonty@gmail.com>
2026-08-04 23:17:04 -0600

    Modify password detector to no capital letter and no special characters as starting state

--- src/main/kotlin/regexfsm/detector/PasswordDetector.kt
+++ src/main/kotlin/regexfsm/detector/PasswordDetector.kt
@@ -1,14 +1,15 @@
 package regexfsm.detector
 
+import regexfsm.config.PasswordRules
 import regexfsm.state.State
-import regexfsm.state.password.PasswordTrackingState
+import regexfsm.state.password.NoCapitalNoSpecialState
 
 class PasswordDetector : AbstractStateMachineDetector() {
     override fun preValidate(input: String): Boolean {
-        return input.length >= 8
+        return input.length >= PasswordRules.MIN_LENGTH
     }
 
     override fun initialState(): State {
-        return PasswordTrackingState()
+        return NoCapitalNoSpecialState()
     }
 }
