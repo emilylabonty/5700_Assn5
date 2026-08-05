6b41f6e08e3d1ea7983d59688bd21cb308c9d9cc
Emily LaBonty <emilylabonty@gmail.com>
2026-08-04 23:30:48 -0600

    Centralize character classifications and import password config file to avoid duplication

--- src/main/kotlin/regexfsm/util/CharacterClassifier.kt
+++ src/main/kotlin/regexfsm/util/CharacterClassifier.kt
@@ -1,5 +1,7 @@
 package regexfsm.util
 
+import regexfsm.config.PasswordRules
+
 object CharacterClassifier {
     fun isDigit(character: String): Boolean {
         return character.length == 1 && character[0] in '0'..'9'
@@ -9,11 +11,20 @@ object CharacterClassifier {
         return character.length == 1 && character[0] in '1'..'9'
     }
 
+    fun isBinaryDigit(character: String): Boolean {
+        return character == "0" || character == "1"
+    }
+
     fun isCapitalLetter(character: String): Boolean {
         return character.length == 1 && character[0] in 'A'..'Z'
     }
 
     fun isPasswordSpecial(character: String): Boolean {
-        return character in setOf("!", "@", "#", "$", "%", "&", "*")
+        return character in PasswordRules.SPECIAL_CHARACTERS
+    }
+
+    fun isSpace(character: String): Boolean {
+        return character == " "
     }
 }
+
